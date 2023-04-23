2785d108ed6dfdee5d4b47091dad6ede6bf21c0c
Krishna Reddy Padala <84383188+PadalaVenkataKrishnareddy@users.noreply.github.com>
2023-04-23 21:40:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,9 +5,15 @@ const express = require('express');
 const Movie = require('./Models/Movie')
 const multer = require('multer');
 const cors = require('cors')
+const AWS = require('aws-sdk');
+const fs = require('fs');
 
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
-mongoose.connect('mongodb://localhost/movies-db', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@sandbox.uzifnvb.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -26,7 +32,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
-// multer configuration
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './uploads')
@@ -38,17 +44,30 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 
-// routes
+
 app.post('/api/movies', upload.single('poster'), async (req, res) => {
   try {
-    const result = await cloudinary.uploader.upload(req.file.path);
+    const fileContent = fs.readFileSync(req.file.path);
+
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: `${Date.now()}-${req.file.originalname}`,
+      Body: fileContent,
+      ContentType: req.file.mimetype,
+      ACL: 'public-read',
+    };
+
+    const s3Data = await s3.upload(params).promise();
+
     const movie = new Movie({
       title: req.body.title,
       director: req.body.director,
       releaseYear: req.body.releaseYear,
-      poster: result.secure_url
+      poster: s3Data.Location,
     });
+
     await movie.save(); 
+
     res.send(movie);
   } catch (error) {
     console.error(error);
